d1844677e9dc9d879af9743628a3e515de4953bd
Jerdik <maratf76@gmail.com>
2022-08-02 07:17:06 +0500

    Добавьте файлы проекта.

---
+++ TodoApi/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+    }
+}
